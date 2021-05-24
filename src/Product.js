a0331bc134efa6805409f2c5d58203feeef1dcd6
Vahe Manukyan <vahemm@outlook.com>
2021-05-24 11:58:13 +0400

    product block

--- src/Product.js
+++ src/Product.js
@@ -10,7 +10,16 @@ function Product() {
           <small>$</small>
           <strong>11.99</strong>
         </p>
+        <div className="product__rating">
+          <p>⭐</p>
+        </div>
       </div>
+
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        alt=""
+      />
+      <button>Add to Basket</button>
     </div>
   );
 }
