dd1a5a758340d6c59cfcbce7b8b10afcd1ef60a2
Vahe Manukyan <vahemm@outlook.com>
2021-05-25 11:59:39 +0400

    adding props

--- src/Home.js
+++ src/Home.js
@@ -12,12 +12,24 @@ function Home() {
           alt=""
         />
         <div className="home__row">
+          <Product
+            title="The lean startup"
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            price={19.9}
+            rating={5}
+          />
           <Product />
         </div>
 
-        <div className="home__row"></div>
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
 
-        <div className="home__row"></div>
+        <div className="home__row">
+          <Product />
+        </div>
       </div>
     </div>
   );
