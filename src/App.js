4df81a2b85fd02787d1f355bf588c20abf2a2af5
Vahe Manukyan <vahe.manukyan@volo.global>
2021-05-27 18:46:25 +0400

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { React } from "react";
+import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      {" "}
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout page</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
