68515c5a25ccff35d2429324939d4f09c246efe3
Vahe Manukyan <vahemm@outlook.com>
2021-06-13 04:43:42 +0400

    adding basket functionals

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Chechout.css";
 import Subtotal from "./Subtotal";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -13,6 +17,16 @@ function Checkout() {
         />
         <div>
           <h2 className="checkout__title">Your shopping Basket</h2>
+          {basket.map((item, index) => (
+            <CheckoutProduct
+              key={index}
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className="checkout__right">
---
+++ src/CheckoutProduct.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
+
+function CheckoutProduct({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+
+  return (
+    <div className="checkoutProduct">
+      <img className="checkoutProduct__image " src={image} />
+      <div className="checkoutProduct__info">
+        <p className="checkoutproduct__title">{title}</p>
+        <p className="checkoutproduct__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p key={i}>⭐</p>
+            ))}
+        </div>
+        <button onClick={removeFromBasket} className="checkoutProduct__button">
+          Remove from Basket
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
