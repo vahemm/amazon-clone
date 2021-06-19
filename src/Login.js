99f524b29da45732365c164a4e6c144d4f56b0a0
Vahe Manukyan <vahemm@outlook.com>
2021-06-19 07:12:45 +0400

    authentication

---
+++ src/Login.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import "./Login.css";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
+
+function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+  const register = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((err) => alert(err.message));
+  };
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          alt=""
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+        />
+      </Link>
+      <div className="login__contanier">
+        <h1>Sign In</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signinButton"
+          >
+            Sign In
+          </button>
+          <p>
+            By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+            Sale. Please see our Privacy Notice, our Cookies Notice and our
+            Interest-Based Ads Notice.
+          </p>
+          <button onClick={register} className="login__registerButton">
+            Create your Amazon Account
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCl8r5OlTk9GInIyOuyW4nlccQmy8NLjLU",
@@ -6,5 +7,11 @@ const firebaseConfig = {
   storageBucket: "clone-604f6.appspot.com",
   messagingSenderId: "197743410656",
   appId: "1:197743410656:web:5472b1c8df696cf93f3f85",
-  measurementId: "G-6FFDEWE30G"
+  measurementId: "G-6FFDEWE30G",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
